101122fadd247a89b20a494efefda7f1cc03f1b7
Patrick Dalton <patrickjdalton1@gmail.com>
2021-02-23 17:57:48 -0600

    Committing react build from previously-wiped repository

---
+++ src/components/PageHead.js
@@ -0,0 +1,10 @@
+function PageHead() {
+    return(
+        <div>
+            <h1>Company Directory</h1>
+            <p>Use the <b>filter</b> textbox below to find employees.</p>
+        </div>
+    )
+}
+
+export default PageHead
